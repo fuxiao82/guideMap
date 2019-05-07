239a7c49f70e7b1699e92ff566c0dc1264d00670
fuxiao82 <2811773791@qq.com>
2019-05-07 23:18:00 +0800

    init

---
+++ guide-map/src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        this is contact
+        contact<input type="text">
+    </div>
+</template>
+<script>
+export default {
+    name: 'contact'
+    
+}
+</script>
+
---
+++ guide-map/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    this is home <input type="text">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+}
+</script>
